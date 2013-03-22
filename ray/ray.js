188500800b30774b4be25dba244475bdb6c5b563
tpatel <thibaut.patel@gmail.com>
2013-03-22 11:08:09 +0100

    Optim raytracer

--- ray/ray.js
+++ ray/ray.js
@@ -12,6 +12,8 @@ function Map() {
 		[0,0,0,0,0,0,1,0,0,0,0,1,3,0],
 		[0,0,0,0,0,0,1,0,0,0,0,0,0,2]
 	];
+	this.width = this.map[0].length;
+	this.height = this.map.length;
 	
 	this.mask = new Array(this.map.length);
 	for(var i=0;i<this.mask.length;i++) {
@@ -41,14 +43,18 @@ Map.prototype.display = function() {
 
 Map.prototype.displayPosition = function(x, y, size) {
 	this.initMask();
-	for(var i in this.map) {
-		for(var j in this.map[i]) {
-			if(Math.abs(y-i)+Math.abs(x-j) < size) {
+	var imin = Math.max(0,y-size);
+	var imax = Math.min(this.height-1,y+size);
+	var jmin = Math.max(0,x-size);
+	var jmax = Math.min(this.width-1,x+size);
+	
+	for(var i=imin; i<=imax; i++) {
+		for(var j=jmin; j<=jmax; j++) {
+			var dx = Math.abs(x-j);
+			var dy = Math.abs(y-i);
+			if(dx+dy <= size) {
 				//Ray trace
-				//console.log(i, j);
 				var x0=x, y0=y;
-				var dx = Math.abs(x-j);
-				var dy = Math.abs(y-i);
 				var sx = 1, sy = 1;
 				if(y >= i) sy = -1;
 				if(x >= j) sx = -1;
@@ -69,20 +75,6 @@ Map.prototype.displayPosition = function(x, y, size) {
 					
 					this.mask[y0][x0] = 1;
 				}
-				/*
-				var end = false;
-				for(var yi=0;Math.abs(yi-y)<=size && !end && yi>=0 && yi <this.map.length;yi+=deltaY) {
-					//console.log(Math.abs(yi-y), deltaY);
-					end = false;
-					for(var xj=0;Math.abs(yi-y)+Math.abs(xj-x)<=size && xj>=0 && xj <this.map[yi].length;xj+=deltaX) {
-						//console.log(this.mask, yi, xj);
-						this.mask[yi][xj] = 1;
-						if(this.map[yi][xj] ==1) {
-							end = true;
-							break;
-						}
-					}
-				}*/
 			}
 		}
 	}
@@ -93,7 +85,7 @@ Map.prototype.displayPosition = function(x, y, size) {
 				str+= '£';
 			} else {
 				var c = this.map[i][j];
-				str += (this.mask[i][j] == 1 ? (c == 1 ? '#' : '.') : ' ');
+				str += (this.mask[i][j] == 1 ? (c == 1 ? '#' : '.') : '?');
 			}
 		}
 		str+='\n';
@@ -107,4 +99,4 @@ var m = new Map();
 //m.initMask();
 m.display();
 
-m.displayPosition(8,3, 8);
+m.displayPosition(4,5, 5);
